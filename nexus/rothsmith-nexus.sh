e416ecce9ca98bad9db0941cd0e03ed5c5500698
Doug Rothauser <drothauser@yahoo.com>
2020-11-09 19:41:18 -0500

    development

--- nexus/rothsmith-nexus.sh
+++ nexus/rothsmith-nexus.sh
@@ -5,28 +5,55 @@
 #    exit 1
 #fi
 
-templateBodyUrl='https://s3.amazonaws.com/rothsmith-cloudformation/rothsmith-nexus.yaml'
+AmiId="ami-0947d2ba12ee1ff75"
+ElbPort="80"
+Ec2Port="8081"
+Scaling='1,1,1'
+Ec2Type="t2.small"
+Ec2Name='Nexus-OSS-Server'
+Ec2Desc="Nexus_Repository_Manager"
+Ec2Owner="drothauser@yahoo.com"
+SvcCode="nexus"
+ElbTier="public"
+Ec2Role="DevOps"
+NexusVersion="3.28.1-01"
+VPCStack="ROTHSMITH-VPC"
+HealthCheckType="ELB"
+stackName="ROTHSMITH-NEXUS"
+
+templateBodyUrl='https://s3.amazonaws.com/rothsmith-iac/nexus/rothsmith-nexus.yaml'
 if [ "$1" == "--file" ]
 then
   templateBodyUrl='file://rothsmith-nexus.yaml'
 fi
 
-stackName="ROTHSMITH-NEXUS"
-
 if aws cloudformation create-stack\
  --capabilities CAPABILITY_IAM \
  --disable-rollback \
- --stack-name $stackName \
- --template-body ${templateBodyUrl}\
+ --stack-name ${stackName}\
+ --template-url ${templateBodyUrl} \
  --parameters\
-    ParameterKey=VPCStack,ParameterValue=\"ROTHSMITH-VPC\" \
-    ParameterKey=S3Bucket,ParameterValue=\"rothsmith-cloudformation\" 
+    ParameterKey=AmiId,ParameterValue=\"${AmiId}\" \
+    ParameterKey=ElbPort,ParameterValue=\"${ElbPort}\" \
+    ParameterKey=Ec2Port,ParameterValue=\"${Ec2Port}\" \
+    ParameterKey=Scaling,ParameterValue=\"${Scaling}\" \
+    ParameterKey=Ec2Type,ParameterValue=\"${Ec2Type}\" \
+    ParameterKey=Ec2Name,ParameterValue=\"${Ec2Name}\" \
+    ParameterKey=Ec2Desc,ParameterValue=\"${Ec2Desc}\" \
+    ParameterKey=Ec2Owner,ParameterValue=\"${Ec2Owner}\" \
+    ParameterKey=SvcCode,ParameterValue=\"${SvcCode}\" \
+    ParameterKey=ElbTier,ParameterValue=\"${ElbTier}\" \
+    ParameterKey=Ec2Role,ParameterValue=\"${Ec2Role}\" \
+    ParameterKey=NexusVersion,ParameterValue=\"${NexusVersion}\" \
+    ParameterKey=VPCStack,ParameterValue=\"${VPCStack}\" \
+    ParameterKey=HealthCheckType,ParameterValue=\"${HealthCheckType}\"
+
 then
    echo "Creating $stackName Stack..."
    if aws cloudformation wait stack-create-complete --stack-name $stackName
    then
       echo "$stackName stack has been created."
-      echo "Updating nexus.rothsmith.net Route 53 record set alias target with ELB host"
+      echo "Updating nexus.rothsmith.net Route 53 record alias target with ELB host"
       ./nexus-route53.sh
    fi
 fi
